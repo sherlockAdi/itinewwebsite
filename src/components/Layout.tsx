9ba95ed9d6abecabbe7d2db004486e9331286843
Aditya Kumar Dwivedi <akdwivedi7355@gmail.com>
2025-06-27 02:37:29 +0530

    mision

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainHeader from './MainHeader';
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 
 const Layout = () => (
   <>
     <MainHeader />
     <Outlet />
+    <Footer />
   </>
 );
 
